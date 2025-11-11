c6b319c10870e72b6633cfdbc0fdf425698cffea
Abdallah Al Kayed <alkayedabdallah1@gmail.com>
2025-11-11 20:36:48 +0300

    debug: env-check endpoint

---
+++ api/env-check.js
@@ -0,0 +1,8 @@
+﻿module.exports = (req,res) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS");
+  if (req.method === "OPTIONS") { res.statusCode = 204; return res.end(); }
+  const has = !!process.env.OPENAI_API_KEY;
+  res.setHeader("Content-Type","application/json; charset=utf-8");
+  return res.end(JSON.stringify({ ok:true, OPENAI_API_KEY_present: has }));
+};
